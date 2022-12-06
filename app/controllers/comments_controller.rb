1a8c390982e89321fb57612680b10ac38a171a00
adamregan53 <adamregan53@gmail.com>
2022-12-06 17:23:14 +0000

    added comment feature for posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = Comment.new(comment_params)
+        @comment.post_id = @post.id
+        @comment.user = User.find(1)
+
+        if @comment.save
+            redirect_to post_path(@post)
+        else
+            render :new, status: :unprocessable_entity
+        end
+
+    end
+
+    def destroy
+    end
+
+    private
+        def comment_params
+            params.require(:comment).permit(:body)
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_095414) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_162606) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.text "commenter"
     t.text "body"
     t.bigint "post_id", null: false
     t.bigint "user_id", null: false
@@ -42,9 +41,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_095414) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+    t.text "email", default: "", null: false
+    t.text "encrypted_password", default: "", null: false
+    t.text "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
