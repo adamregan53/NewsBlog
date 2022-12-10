d7827655351f2620fc78097f604766964b27a98e
adamregan53 <adamregan53@gmail.com>
2022-12-10 12:08:05 +0000

    testing changes to render.yml file

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -3,7 +3,6 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can create comment' do
+  end
 end
